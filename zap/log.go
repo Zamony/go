a40be551ac2c0c618b3b93e9b1f37839b71f34f7
Nikita Mochalov <mochalov.n@yandex.ru>
2024-11-05 21:57:26 +0300

    Add slog attributes

--- zap/log.go
+++ zap/log.go
@@ -66,3 +66,39 @@ func Error(err error) slog.Attr {
 
 	return slog.Group("", attr, slog.String("errstack", stack))
 }
+
+func Any(key string, value any) slog.Attr {
+	return slog.Any(key, value)
+}
+
+func Bool(key string, value bool) slog.Attr {
+	return slog.Bool(key, value)
+}
+
+func Duration(key string, value time.Duration) slog.Attr {
+	return slog.Duration(key, value)
+}
+
+func Float64(key string, value float64) slog.Attr {
+	return slog.Float64(key, value)
+}
+
+func Int(key string, value int) slog.Attr {
+	return slog.Int(key, value)
+}
+
+func Int64(key string, value int64) slog.Attr {
+	return slog.Int64(key, value)
+}
+
+func String(key, value string) slog.Attr {
+	return slog.String(key, value)
+}
+
+func Time(key string, value time.Time) slog.Attr {
+	return slog.Time(key, value)
+}
+
+func Uint64(key string, value uint64) slog.Attr {
+	return slog.Uint64(key, value)
+}
